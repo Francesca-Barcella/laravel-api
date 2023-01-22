e9db8ec44b1bf1c79c8960f89c6e71b4ca4bd821
Francesca-Barcella <pollon.fra@gmail>
2023-01-22 11:33:57 +0100

    add api projects with type & technology

---
+++ app/Http/Controllers/API/ProjectController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        //SINTAX INDEX 1
+        return response()->json([
+            //'success'=> 'true' => serve per far vedere all'utente che è andata a buon fine la chiamata
+            'success' => 'true',
+            //'results' => Project::orderByDesc('id')->paginate(5)
+            //aggiungo il metodo with() per correlare anche type e technologies
+            'results' => Project::with('type', 'technologies')->orderByDesc('id')->paginate(5)
+        ]);
+
+        //SINTAX INDEX 2
+        //return Project::orderByDesc('id')->get();
+
+        //SINTAX INDEX 2 + paginate
+        //return Project::orderByDesc('id')->paginate(5);
+    }
+}
