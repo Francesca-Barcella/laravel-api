405cb7052449f562d609e90a02aebc55cafd0514
Francesca-Barcella <pollon.fra@gmail>
2023-01-25 12:48:33 +0100

    add mail - work-in-progress

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,10 @@
+<h1>ciao admin</h1>
+<p>
+    Hai un nuovo messaggio <br>
+
+    Nome:{{$lead->name}}<br>
+    Nome:{{$lead->mail}}<br>
+    Nome:{{$lead->message}}<br>
+
+
+</p>
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\ProjectController;
+use App\Http\Controllers\API\LeadController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -20,3 +21,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/projects', [ProjectController::class, 'index']);
 Route::get('/projects/{project:slug}', [ProjectController::class, 'show']);
+
+//sicuro ci serve [ProjectController::class, 'show'] -> [LeadController::class, 'store']
+// poi l'url '/contacts'
+// metodo sarà post perchè in questo caso stiamo inviando dati
+Route::post('/contacts', [LeadController::class, 'store']);
